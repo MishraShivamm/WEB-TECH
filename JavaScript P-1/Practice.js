025224a12f917af575caf7beb26916f164f93432
Shivam <mishrashiv1708@gmail.com>
2025-01-26 12:50:06 +0530

    JS

--- JavaScript P-1/Practice.js
+++ JavaScript P-1/Practice.js
@@ -7,4 +7,28 @@ let english = 100;
 let math = 90;
 let avg = (hindi+english+math)/3;
 console.log(avg);
- 
+
+/* Find the errors in the following code.
+
+let 1age = 5;
+let 2age = 10;
+
+The error in this code is that the variables cannot start with Number . so
+the correct way of writing it will be 
+
+let age1 = 5;
+let age2 = 10;
+
+
+b) let marks= 75;
+let isPass = True;
+
+in this the marks is declared correctly and isPass is not because writing True like this is not the correct way . we can write it like this true. 
+
+c) let isPass = "true";
+
+This is also not the correct way of writing because we are here declaring a boolean so we can write true without the quotes, which will gonna be the correct way.
+
+*/
+
+
