79a55dc7a4e2e166a246541185fb6e272188aed6
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-02-03 11:17:41 +0000

    String

---
+++ JavaScript P-3/Practice.js
@@ -0,0 +1,4 @@
+let msg = "help!";
+let newmsg = msg.trim();
+let upper = msg.toUpperCase()
+console.log(newmsg, upper);
--- JavaScript P-3/Stringmethod.js
+++ JavaScript P-3/Stringmethod.js
@@ -20,3 +20,5 @@ console.log(find);
 //Slice : Returns the part of original string as a new string. str.slice(start,end); or str.slice(start); and if we pass str.slice(-num) means if we pass negative value then this method will subtract the value which we provided from the original length of the string.  for example : let str ="abcdefgh"; str.slice(2); 
 
 //Replace : searches the value in the string and returns the new string with the value replaced. for example: let str : qwerty; str.replace(we, ew);
+
+//Repeat: returns a string with the number of copies of a string. for  example: let str="abc"; str.repeat(3);
