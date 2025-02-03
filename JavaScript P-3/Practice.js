7d27155e28425a76bc95b51b1f8424b01bfdc302
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-02-03 11:31:52 +0000

    String

--- JavaScript P-3/Practice.js
+++ JavaScript P-3/Practice.js
@@ -2,3 +2,15 @@ let msg = "help!";
 let newmsg = msg.trim();
 let upper = msg.toUpperCase()
 console.log(newmsg, upper);
+
+
+
+let name = "apnaCollege";
+let newname=name.slice(4,9);
+let nameindex = name.indexOf("na");
+let repname = name.replace("apna","Our");
+console.log(newname,nameindex,repname);
+
+let sep = name.slice(4);
+let rep = sep.replace("l","t");
+console.log(sep,rep);
