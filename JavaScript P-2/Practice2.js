2f652fda69db9d42036363eeb2a7f09571935ba4
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-02-03 10:19:25 +0000

    JS

---
+++ JavaScript P-2/Practice2.js
@@ -0,0 +1,16 @@
+let size = prompt("Enter the box size");
+if(size == "XL"){
+    console.log("Price is 250");
+}
+else if(size == "L"){
+    console.log("Price is 200");
+}
+else if(size == "M"){
+    console.log("Price is 100");
+}
+else if(size == "S"){
+    console.log("Price is 50");
+}
+else{
+    console.log("Enter coorect box Size");
+}
