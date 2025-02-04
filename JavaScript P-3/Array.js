8751680f6afa0745728beb2b1b99353bf9da0986
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-02-04 10:08:05 +0000

    Array

---
+++ JavaScript P-3/Array.js
@@ -0,0 +1,13 @@
+//Arrays are mutable i.e we can change it's values.
+
+//Push and Pop : we use push when we have to add a value or something inside the araay  in last.For example :  let arr = ["a","b","c","d"]; arr.push("e"); 
+// We use Pop to remove last element from the array. 
+
+/* And if we have to add the value to the start and remove it from start we'll use Unshift and Shift.
+Unshift :We use it to add the value to  start of Array. 
+Shift: Deletes the value from start and returns the array . */ 
+
+let array = ["january","july","march","august"];
+array[0]="july";
+array[1]="june";
+console.log(array);
