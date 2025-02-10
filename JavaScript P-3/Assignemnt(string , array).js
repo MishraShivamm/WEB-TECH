116eb5925bac024d289a26649ee54e342c44f70e
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-02-10 07:44:20 +0000

    String , Array

--- JavaScript P-3/Assignemnt(string , array).js
+++ JavaScript P-3/Assignemnt(string , array).js
@@ -8,18 +8,23 @@ let n = 3;
 let posarray = arr.slice(arr.length-n);
 console.log(posarray);
 
-let str = "";
-if(str ==0){
-    console.log("string is empty");
-} else{
-    console.log("string is not empty");
-}
+// let str = "";
+// if(str ==0){
+//     console.log("string is empty");
+// } else{
+//     console.log("string is not empty");
+// }
 
-let string = "QWERty";
-let index= 4;
-if(string[index] == string[index].toLowerCase()){
-    console.log("character is lower case");
-} else{
-    console.log("character is upper case");
-}
+// let string = "QWERty";
+// let index= 4;
+// if(string[index] == string[index].toLowerCase()){
+//     console.log("character is lower case");
+// } else{
+//     console.log("character is upper case");
+// }
+
+let str = "            qwertyuio";
+console.log(`original string is ${str}`);
+console.log(`new string is ${str.trim()}`);
+
 
