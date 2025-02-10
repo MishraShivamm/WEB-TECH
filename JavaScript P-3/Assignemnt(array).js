44ee73cdcc9961e3619f8468908ac33f6eca1ba9
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-02-10 07:28:43 +0000

    String , Array

--- JavaScript P-3/Assignemnt(array).js
+++
@@ -1,5 +0,0 @@
-let arr = [7,9,0,-2];
-let n = 3;
-
-let posarray = arr.slice(0,n);
-console.log(posarray);
---
+++ JavaScript P-3/Assignemnt(string , array).js
@@ -0,0 +1,16 @@
+let arr = [7,9,0,-2];
+let n = 3;
+
+// let posarray = arr.slice(0,n);
+// console.log(posarray);
+
+
+let posarray = arr.slice(arr.length-n);
+console.log(posarray);
+
+let str = "";
+if(str ==0){
+    console.log("string is empty");
+} else{
+    console.log("string is not empty");
+}
