335ad9fc25c605c29203fdbbd2f77aa77f88ddfd
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-02-10 06:51:54 +0000

    String , Array

--- JavaScript P-3/Practice(Nested Array).js
+++ JavaScript P-3/Practice(Nested Array).js
@@ -1,3 +1,3 @@
 let arr = [['x','','o'],['','x',''],['o','x','o']];
-arr[0][1] = 'o';
+arr[0][1] = 'o'; // It will change the value of first nested array i.e['x','','o'] to ['x','o','o'];
 console.log(arr);
