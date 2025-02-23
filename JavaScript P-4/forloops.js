7ee68c3c27cf8b9d7fecf64af3474ac6fc1431cc
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-02-23 08:44:16 +0000

    Loops

--- JavaScript P-4/forloops.js
+++ JavaScript P-4/forloops.js
@@ -1,7 +1,14 @@
 /* Loops
  Used to iterate a piece of Code 
 */
+//Print Odd numbers
 
 for (let i = 1; i <=10;i=i+2){
     console.log(i)
+}
+
+//Print Even numbers
+
+for (let even = 0; even <=10; even = even+2){
+    console.log(even);
 }
