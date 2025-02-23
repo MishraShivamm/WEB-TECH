20f065cecb6fc8f7fecc0d6d47d066cd935270b9
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-02-23 17:45:28 +0000

    Loops(For)

---
+++ JavaScript P-4/nestedforloop.js
@@ -0,0 +1,22 @@
+for(let i = 1; i<=3;i++){
+    console.log(i);
+    for(let j=1;j<=3;j++){
+        console.log(j);
+    }
+}
+/* output of this will be 
+1
+1
+2
+3
+2
+1
+2
+3
+3
+1
+2
+3
+ because first the outer loop will run one time then the inner loop and the again the outer loop and then inner loop and same will be repeated again , till the condition is satisfied*/
+
+ 
