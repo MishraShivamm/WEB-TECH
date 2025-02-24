0766126f97f556be0c0fa7ed95ddee2681e0a0b4
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-02-24 19:23:09 +0000

    Loops(For , While)

--- JavaScript P-4/for of.js
+++ JavaScript P-4/for of.js
@@ -8,3 +8,12 @@ let ch = "jishi";
 for(char of ch){
     console.log(char);
 }
+
+//Nested For of Loop
+
+let heros =  [["ironman","Captain america","Hulk"],["superman","Batman","Wonder women"]];
+for(list of heros){
+    for(hero of list){
+        console.log(hero);
+    }
+}
