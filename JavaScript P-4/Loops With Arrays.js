1e814006cd205381f14e6871e3f73754094408d3
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-02-24 19:03:31 +0000

    Loops(For , While)

--- JavaScript P-4/Loops With Arrays.js
+++ JavaScript P-4/Loops With Arrays.js
@@ -6,3 +6,10 @@ for (let i = 0; i < fruits.length; i++) {
 
 //Nested Array
 
+let heroes = [["ironman","Captain america","Hulk"],["superman","Batman","Wonder women"]];
+for(let i = 0; i<heroes.length;i++){
+    console.log(heroes[i]);
+    for(let j = 0;j<heroes[i].length;j++){
+        console.log(heroes[i][j]);
+    }
+}
