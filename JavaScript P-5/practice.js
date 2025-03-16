9c790f5322403fad118613e7440d0db5f9b86724
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-03-16 11:16:16 +0000

    Object

---
+++ JavaScript P-5/practice.js
@@ -0,0 +1,10 @@
+//Generate a random number between 1 to 100
+
+let num = Math.floor(Math.random()*100)+1;
+console.log(num);
+
+
+//Generate a random number between 1 to 5
+
+let ran = Math.floor(Math.random()*5)+1;
+console.log(ran);
