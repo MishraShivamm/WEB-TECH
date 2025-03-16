8a189ec98a7be3cf01fbcf8fa6fe2fc1c4edd4f0
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-03-16 11:42:11 +0000

    Object

---
+++ JavaScript P-5/assignmen Objectt.js
@@ -0,0 +1,21 @@
+let random = Math.floor(Math.random()*6)+1;
+console.log(random);
+
+const car ={
+    name : "BMW",
+    model: "X-series",
+    color: "Blue"
+};
+console.log(car.name);
+
+const person = {
+    name:"Jishi",
+    age: 23,
+    city:"Delhi"
+};
+
+person.city="New York";
+person.country="USA";
+
+console.log(person);
+
