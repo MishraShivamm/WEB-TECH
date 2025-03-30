59aa99bb3bd4c57ff5c23f01ee77f42b8d899d3c
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-03-30 07:02:21 +0000

    Object

--- JavaScript P-5/GuessGame.js
+++ JavaScript P-5/GuessGame.js
@@ -1,8 +1,16 @@
 let max =prompt("Enter Max number!");
 let guess = Math.floor(Math.random()*max)+1;
 
-let user = prompt("Enter your Guess");
+let user_guess = prompt("Enter your Guess");
 
 while(true){
-    
+    if(guess== "quit"){
+        console.log("User Quit");
+        break;
+    } else if(guess == user_guess){
+        console.log("You Guessed it Right");
+        break;
+    }else{
+        console.log("Guess Again");
+    }
 }
