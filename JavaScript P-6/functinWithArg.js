5edd9299b1896ddc109fb06db4e6553e9b3d4e38
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-03-30 10:18:10 +0000

    Functions

---
+++ JavaScript P-6/functinWithArg.js
@@ -0,0 +1,4 @@
+function avg(num1,num2,num3){
+    console.log((num1+num2+num3)/3);
+}
+avg(3,4);
--- JavaScript P-6/functions.js
+++ JavaScript P-6/functions.js
@@ -2,3 +2,11 @@ function poem(){
     console.log("Twinkle Twinkle Little Star , How I wonder what you are , Up above the world so high , like a diamond in the sky");
 }
 poem();
+
+function rollDice(){
+    for(let i=1;i<=6;i++){
+        console.log(i);
+        break;
+    }
+}
+rollDice();
