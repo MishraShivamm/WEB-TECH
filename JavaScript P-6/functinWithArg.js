ce16172b1d83b58a38218a84edff4b631987f360
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-03-30 10:23:02 +0000

    Functions

--- JavaScript P-6/functinWithArg.js
+++ JavaScript P-6/functinWithArg.js
@@ -2,3 +2,14 @@ function avg(num1,num2,num3){
     console.log((num1+num2+num3)/3);
 }
 avg(3,4,8);
+
+
+// Create table using function
+
+function table ( num){
+    for(let i = 1;i<=10;i++){
+        console.log(num*i);
+    }
+}
+
+table(5);
