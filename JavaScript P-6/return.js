0a381bbc106f3b508ac829f70eedfd19ca9814f5
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-04-06 10:32:06 +0000

    Functions

--- JavaScript P-6/return.js
+++ JavaScript P-6/return.js
@@ -2,6 +2,16 @@
 Return keyword is used to return some value from a function
 
 input -> function(does some work) -> output
-
-
 */
+
+//Create a function that calculate the sum from 1 to n
+
+function getSum(n){
+    let sum = 0;
+    for(let i=1;i<=n;i++){
+        sum = sum + i;
+    }
+    return sum;
+}
+console.log(getSum(7));
+
