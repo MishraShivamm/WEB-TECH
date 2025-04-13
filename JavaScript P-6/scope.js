00b4fcbe0ad96c96fe96c87b6577be9932fce020
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-04-13 10:11:30 +0000

    Scope

--- JavaScript P-6/scope.js
+++ JavaScript P-6/scope.js
@@ -12,7 +12,21 @@ Scopre determines the accessibility of variables , functions and objects from di
 
 //Block Scope : The variable declared inside {} the block cannot be accessed outside the block.
 
-//Lexical Scope : A variable declared out a function can be accessible inside another function after the variable declaration. [The reverse is not true.]
+//Lexical Scope : A variable declared out a function can be accessible inside another function after the variable declaration. [The opposite is not true.]
+
+
+let greet = "hello";
+
+function changeGreet(){
+    greet = "namaste";
+    console.log(greet);
+    function innerGreet(){
+        console.log(greet);
+    }
+    innerGreet();
+}
+console.log(greet);
+changeGreet();
 
 
 
