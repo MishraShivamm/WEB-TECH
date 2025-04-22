c80e84514781f2b0280a41458f98ebf1e1ce2653
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-04-22 06:44:44 +0000

    Higherorder Functions

--- JavaScript P-6/functionexpressionn/higherorderfunction(return).js
+++ JavaScript P-6/functionexpressionn/higherorderfunction(return).js
@@ -1,12 +1,18 @@
 /* Factory function : The function which creates new functions is called factory function.
 */
 
-let odd = function(n){
+// let odd = function(n){
     // if(n%2 !=0){
     //     console.log("N is ODD");
     // } else if(n%2 == 0){
     //     console.log("N is even");
     // }
+// }
+// odd(3);
+
+
+// Other way of doing it is 
+let odd = function (n){
     console.log(!(n%2==0));
 }
-odd(3);
+odd(8);
