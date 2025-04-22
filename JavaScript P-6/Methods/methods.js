3d578b08790a2e40731c2af80d28c0b4a3015ecc
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-04-22 07:27:47 +0000

    Methods

---
+++ JavaScript P-6/Methods/methods.js
@@ -0,0 +1,6 @@
+/* Methods : Actions that can be performed on an object.
+    * The functions which are defined inside an object are called methods.
+    * A function that is a property of an object is called method.
+    * All methods are functions, but not all functions are methods.
+*/
+
