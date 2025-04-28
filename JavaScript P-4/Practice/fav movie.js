9a2200983a3e53e107105a127f7740b4e6f866ec
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-04-28 05:05:10 +0000

    Practice Loops

--- JavaScript P-4/Practice/fav movie.js
+++ JavaScript P-4/Practice/fav movie.js
@@ -1,11 +1,19 @@
-let favMovie = "cruel intentions";
-let guess = prompt("Enter your prompt");
-while((guess !=favMovie)&& (guess !="quit")){
-    console.log("Wrong guess, try again");
-    guess= prompt("Guess again");
-} if(guess == favMovie){
-    console.log("You guessed it right");
-    alert("You guessed it right");
-}else if (guess == "quit"){
-    console.log("You quit , you loose");
+// let favMovie = "cruel intentions";
+// let guess = prompt("Enter your prompt");
+// while((guess !=favMovie)&& (guess !="quit")){
+//     console.log("Wrong guess, try again");
+//     guess= prompt("Guess again");
+// } if(guess == favMovie){
+//     console.log("You guessed it right");
+//     alert("You guessed it right");
+// }else if (guess == "quit"){
+//     console.log("You quit , you loose");
+// }
+
+
+
+let arr = ["mango","lichi","apple"];
+
+for(let i=0;i<arr.length;i++){
+    console.log(i,arr[i]);
 }
