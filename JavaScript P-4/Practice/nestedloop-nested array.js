4a8bdc08df0fcc1609d47f84586035c8124641a7
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-04-28 07:09:21 +0000

    Loops Practice

---
+++ JavaScript P-4/Practice/nestedloop-nested array.js
@@ -0,0 +1,15 @@
+let arr = [["ironman","Batman","Chota Bheem"],["Raju","Captain America","Hulk"]];
+for(let i =0;i<arr.length;i++){
+    console.log(arr[i]);
+    for(let j=0;j<arr[i].length;j++){
+        console.log(j,arr[i][j]);        
+    }
+}
+
+
+let student = [["shivam",99],["jiya",100]];
+for(let i=0; i<student.length;i++){
+    for(let j=0;j<student[i].length;j++){
+        console.log(student[i][j]);
+    }
+}
