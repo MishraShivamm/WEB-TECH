388c6864b7ac8d6d6f6afda56ccdbca08106343a
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-04-29 05:24:40 +0000

    Assignment Loop

--- JavaScript P-4/Assignment Loop.js
+++ JavaScript P-4/Assignment Loop.js
@@ -1,4 +1,5 @@
-//Question 1
+/*Question 1
+Write a JS program to delete all occurrences of element ‘num’ in a given array.Example:if arr=[1,2,3,4,5,6,2,3] & num = 2 Result should be arr=[1,3,4,5,6,3] */
 
 let arr = [1,2,3,4,5,6,7,8,9];
 let num = 3;
