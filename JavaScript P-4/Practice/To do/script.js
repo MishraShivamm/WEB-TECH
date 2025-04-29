9ba244cace8b855621dfb3fb88f117f1c190bdd7
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-04-29 04:54:52 +0000

    Loops Practice

--- JavaScript P-4/Practice/To do/script.js
+++ JavaScript P-4/Practice/To do/script.js
@@ -1,1 +1,19 @@
+let toDo = ["add", "sub", "mul", "del"];
 
+let req = prompt("Enter your request");
+
+while(true){
+    if(req =="quit"){
+        console.log("You are quitting the app");
+        break;
+    }
+    if(req =="add"){
+        toDo.push("avg");
+    } else if ( req == "delete"){
+        toDo.pop();
+    }else if(req == "list"){
+        for(list of toDo){
+            console.log(list);
+        }
+    }
+}
