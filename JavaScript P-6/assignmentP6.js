888b5790913c06092ff98a3ea7663b8cc4537940
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-04-30 06:24:01 +0000

    Assignment Functions

---
+++ JavaScript P-6/assignmentP6.js
@@ -0,0 +1,12 @@
+//Question 1: Write a function to return all the array elements greater then the number.
+
+let arr = [1,2,5,8,3,6,8,3,643,89,10];
+let num = 8;
+function getElements(arr, num){
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]>=num){
+            console.log(arr[i]);
+        }
+    }
+}
+getElements(arr,num);
