c6a54d29c0a7e1d445a79d551e8bdc979c374836
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-05-01 09:57:04 +0000

    This

--- JavaScript P-7/this.js
+++ JavaScript P-7/this.js
@@ -1,3 +1,5 @@
+//This keyword: "This" keywords refers to an object that is executing the current piece of code.
+
 const obj = {
     name:"shivi",
     age:"24",
@@ -7,3 +9,6 @@ const obj = {
     }
 }
 console.log(obj.getname());
+
+let abc = 23;
+console.log(abc);
