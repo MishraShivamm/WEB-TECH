5924e5084410b9781bcab6988fdeb222dc1fa56f
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-05-01 10:09:42 +0000

    Try n Catch

---
+++ JavaScript P-7/try n catch.js
@@ -0,0 +1,2 @@
+//Try : "the try statement allows you to define a block for errors while it is being executed.
+//Catch : "the catch statement allows you to define a block of code to be executed if an error occurs in try block".
