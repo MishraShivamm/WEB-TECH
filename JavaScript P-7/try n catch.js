b35f64fb8f69b6dfe7ba66ad04514d2535e6d747
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-05-02 05:27:31 +0000

    Try n Catch

--- JavaScript P-7/try n catch.js
+++ JavaScript P-7/try n catch.js
@@ -2,3 +2,26 @@
 //Catch : "the catch statement allows you to define a block of code to be executed if an error occurs in try block".
 
 
+/* without try and catch
+
+console.log("Hello");
+console.log(a);
+console.log("a");
+
+
+In the above code we'll only get hello as output and then an error that a is not defined
+*/
+
+/* With Try and Catch
+
+console.log("Hello");
+try{
+console.log(A)
+}
+catch{
+console.log("A")
+}
+
+In this we'll get the output as hello and A as the code in try block will catch the error and will keep the code running.
+*/
+
