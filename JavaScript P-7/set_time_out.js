a803ee3054bee6a1471ddb3bdbaf57d96935d422
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-05-03 18:28:14 +0000

    Timeout

--- JavaScript P-7/set_time_out.js
+++ JavaScript P-7/set_time_out.js
@@ -4,3 +4,13 @@ setTimeout(() => {
 }, 5000);
 
 console.log("Ho gya");
+
+/* syntax of set time out 
+
+setTimeout( function , time);
+
+setTimeout(() => {
+    console.log("ABC");
+}, 5000);
+
+*/
