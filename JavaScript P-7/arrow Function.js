a82443ca253860d1c864b7b39d536ccf5057e54f
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-05-03 07:22:27 +0000

    Arrow Functions

--- JavaScript P-7/arrow Function.js
+++ JavaScript P-7/arrow Function.js
@@ -1,4 +1,17 @@
 const sum = (a,b)=>{
     console.log(a+b);
-}
+};
 sum(5,6);
+/* There are other ways of doing the syntax of arrow function
+
+const sum = () =>{}
+
+    The above line code is only used when there's no argument passed 
+    
+
+const sum = a =>{};
+
+ This code is used when only a single argument is passed
+
+*/
+
