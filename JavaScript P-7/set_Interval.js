ef8b627e19515089862cddc5c11daff578ab2642
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-05-03 18:43:54 +0000

    Interval

---
+++ JavaScript P-7/set_Interval.js
@@ -0,0 +1,16 @@
+//setInterval
+
+console.log("ABC");
+setInterval(() => {
+    console.log("Hello", "Hello");
+}, 5000);
+
+console.log("Hllo Hllo");
+
+
+/* The difference between setTimeout and setInterval is that:
+setTimeout run the code only once .
+
+but, setinterval keeps running the code in that interval again and again untill it is stopped.
+
+*/
