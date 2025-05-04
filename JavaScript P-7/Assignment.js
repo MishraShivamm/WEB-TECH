05af97f9f5ee97bfa11de587e3465389c4a6c171
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-05-04 11:38:05 +0000

    Assignment

---
+++ JavaScript P-7/Assignment.js
@@ -0,0 +1,11 @@
+//Write an arrow function named arrayAverage that accepts an array of numbers and returns the average of those numbers.
+
+const arrayAverage = (arr)=>{
+    let sum = 0;
+    for(let i=0;i<arr.length;i++){
+        sum = sum + arr[i];
+    }
+    let avg = sum/arr.length;
+    return avg;
+}
+console.log(arrayAverage([2,3,4,5,6,7]));
