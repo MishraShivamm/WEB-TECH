d275be844bffa639a92d7c8cc3bca080fd8024e7
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-05-04 10:56:35 +0000

    Practice of Functions , Arrow Functions and This

--- JavaScript P-7/Practice Q.js
+++ JavaScript P-7/Practice Q.js
@@ -1,6 +1,9 @@
-// Write an arrow function that returns the square of n
+// Write an arrow function that returns the square of n.
 
 const square = (n)=> {
     console.log(n*n);
 }
 square(4);
+
+//Write a function that prints "Hello World" 5 times at intervals of 2s each.
+
