21c68c7fc5eeed2bbd4064a36c365941bec5c821
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-05-04 11:06:11 +0000

    Practice of Functions,, Arrow Functions and This

--- JavaScript P-7/Practice Q.js
+++ JavaScript P-7/Practice Q.js
@@ -7,3 +7,14 @@ square(4);
 
 //Write a function that prints "Hello World" 5 times at intervals of 2s each.
 
+const print = () => {
+    let count = 0;
+    const intervals = setInterval(() => {
+        console.log("Hello World");
+        count++;
+        if(count == 5){
+            clearInterval(intervals);
+        };
+    }, 2000);
+}
+print();
