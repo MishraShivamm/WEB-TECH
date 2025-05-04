eb4e81a943fe583dad532a0a40fe39003a7502d8
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-05-04 11:11:50 +0000

    Practice

--- JavaScript P-7/Practice Q.js
+++ JavaScript P-7/Practice Q.js
@@ -7,14 +7,24 @@ square(4);
 
 //Write a function that prints "Hello World" 5 times at intervals of 2s each.
 
-const print = () => {
-    let count = 0;
-    const intervals = setInterval(() => {
-        console.log("Hello World");
-        count++;
-        if(count == 5){
-            clearInterval(intervals);
-        };
-    }, 2000);
-}
-print();
+// const print = () => {
+//     let count = 0;
+//     const intervals = setInterval(() => {
+//         console.log("Hello World");
+//         count++;
+//         if(count == 5){
+//             clearInterval(intervals);
+//         };
+//     }, 2000);
+// }
+// print();
+
+//Other way of doing the same problem is
+
+let id = setInterval(() => {
+    console.log("Hello World");
+}, 2000);
+
+setTimeout(() => {
+    clearInterval(id);
+}, 10000);
