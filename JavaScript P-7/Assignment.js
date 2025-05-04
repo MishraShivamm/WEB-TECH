826b5241f59a00a4f7e96e07f657e4e79ebbebcf
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-05-04 12:19:40 +0000

    Assignment

--- JavaScript P-7/Assignment.js
+++ JavaScript P-7/Assignment.js
@@ -25,12 +25,29 @@ console.log(even(5));
 
 //What will be the output of this code.
 
-const object = {
-    message: "Hello World",
-    logMessage (){
-        console.log(this.message);
-    }
-};
-setTimeout(object.logMessage,1000);
+// const object = {
+//     message: "Hello World",
+//     logMessage (){
+//         console.log(this.message);
+//     }
+// };
+// setTimeout(object.logMessage,1000);
 
 // It will print undefined
+
+
+// What will be the output of this code
+
+let length = 4;
+
+function callback()  {
+    console.log(this.length);
+}
+
+const object = {
+    length: 5,
+    method (callback){
+        callback();
+    },
+};
+object.method(callback,1,2);
