67feaa264f7f61dfe384fe6f8f659763fde73a18
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-05-05 09:39:47 +0000

    PRACTICE

---
+++ JavaScript P-7/Practice /practice6597*.js
@@ -0,0 +1,15 @@
+// Q1:  Convert a traditional function to an arrow function.
+
+//Normal function
+// function sample (){
+//     console.log("sample");  
+// }
+// sample();
+
+//Arrow Function 
+// const sample = () ={ };
+// sample();
+
+/*
+ 
+*/
