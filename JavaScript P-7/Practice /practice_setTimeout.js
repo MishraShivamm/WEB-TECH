dc10f2b06db20cee37a657b21537f8f6afa6146b
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-05-05 10:48:13 +0000

    Practice

--- JavaScript P-7/Practice /practice_setTimeout.js
+++ JavaScript P-7/Practice /practice_setTimeout.js
@@ -4,3 +4,16 @@ setTimeout(() => {
 }, 2000);
 */
 
+/* Display countdown from 3 to 1 with 1-second delay between each.
+
+for(let i=1;i<=3;i++){
+    setTimeout(() => {
+       console.log(i); 
+    }, (4-i)*1000);
+}
+    
+*/
+
+/* 
+*/
+
