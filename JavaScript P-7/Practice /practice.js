a1aa406974a62134eec930744c892a05ba48d3fc
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-05-05 09:52:17 +0000

    Prctice

--- JavaScript P-7/Practice /practice.js
+++ JavaScript P-7/Practice /practice.js
@@ -17,3 +17,12 @@ const arr = [2,4,6,8];
 const doubled = arr.map(num=> num*2);
 console.log(doubled());
 */
+
+
+/* Write an arrow function that checks if a string is a palindrome.
+const ispallindrome = str =>str.split(' ').reverse(' ').join(' ');
+console.log(ispallindrome("reverse"));
+*/
+
+const ispallindrome = str =>str.split(' ').reverse(' ').join(' ');
+console.log(ispallindrome("reverse"));
