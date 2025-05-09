eb33ddd377e31b93d3cac52c354e95db7381564b
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-05-09 18:38:51 +0000

    Array Methods

--- JavaScript P-8/practice.js
+++ JavaScript P-8/practice.js
@@ -1,8 +1,19 @@
 //check if all the numbers in an array are multiple of 10 or not.
 
-let arr = [10,70,90,10,50,80,90];
+let arr = [10,70,90,10,5,80,90];
 let check = arr.every((el)=> el%10 == 0);
 console.log(check);
 
 //Create a function too find minimum in an array
 
+let check_arr = arr.reduce((min,el)=>{
+    if(min>el){
+        return el;
+    }
+    else{
+        return min;
+    }
+
+})
+
+console.log(check_arr);
