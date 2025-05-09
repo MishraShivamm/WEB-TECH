e3aad72e825ade8aa40821d4af68bab393d82484
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-05-09 11:15:04 +0000

    Array Methods

--- JavaScript P-8/reduce.js
+++ JavaScript P-8/reduce.js
@@ -1,3 +1,16 @@
 let nm = [1,2,3,4,5];
 let final_nm = nm.reduce((res,el) => res+el);
 console.log(final_nm);
+
+
+//Find maximum of a array using REDUCE method
+
+let arr = [1,2,5,3,4,7,6];
+let maxArr = arr.reduce((max,el) => {
+    if(el>max){
+        return el;
+    }else{
+        return max;
+    }
+});
+console.log(maxArr);
