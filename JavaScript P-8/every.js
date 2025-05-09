1c2399a8dc28ca643207e2adb47ae4abd15df411
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-05-09 07:13:36 +0000

    Array Methods

--- JavaScript P-8/every.js
+++ JavaScript P-8/every.js
@@ -1,3 +1,6 @@
 //Every : it returns true if EVERY element of an array gives true for some functon else returns false.
 //Syntax : arr.every(some function definition or name)
 
+let arr = [2,4,6,8];
+let new_arr = arr.every((el) => el%2 == 0);
+console.log(new_arr);
