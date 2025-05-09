078af4f4ad35657455400c10200a3adbf65205ec
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-05-09 06:49:39 +0000

    Array Methods

---
+++ JavaScript P-8/every.js
@@ -0,0 +1,3 @@
+//Every : it returns true if EVERY element of an array gives true for some functon else returns false.
+//Syntax : arr.every(some function definition or name)
+
--- JavaScript P-8/fliter.js
+++ JavaScript P-8/fliter.js
@@ -1,1 +1,8 @@
 //Flter : let abc = arr.filter(some function definition or name)
+//I also creates a new array but in this if the output is True only then it'll be added to new array else it won't be added to the new array.
+
+let array = [1,4,6,2,7,9,29];
+
+let new_arr = array.filter((num) => 
+    {return num % 2 == 0});
+console.log(new_arr);
