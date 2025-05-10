8009674f865079dbdf8f710023a7b31c7704bea5
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-05-10 10:31:16 +0000

    Array Methods

--- JavaScript P-8/Spread/spread(Object literals).js
+++ JavaScript P-8/Spread/spread(Object literals).js
@@ -5,3 +5,10 @@ let obj = {
 
 let objCopy ={...obj,id:123};
 console.log(objCopy);
+
+let arr = [1,2,3,4,5,6];
+let obj_arr = {...arr};
+console.log(obj_arr);
+let str = "Hello";
+let obj_str = {...str};
+console.log(obj_str);
