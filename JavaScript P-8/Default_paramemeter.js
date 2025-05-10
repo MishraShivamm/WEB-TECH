64cbcb22f282bf96b8d33536e544e013407b2785
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-05-10 05:09:24 +0000

    Array Methods

--- JavaScript P-8/Default_paramemeter.js
+++ JavaScript P-8/Default_paramemeter.js
@@ -4,3 +4,15 @@ function mul(a,b=9){
     console.log(a*b);
 }
 mul(3);
+
+/* If suppose we do this 
+function mul(a=5,b){
+    console.log(a*b);
+}
+mul(3);
+
+So the output of this will be "NaN" 
+
+Reason for this is that value is assigned in order that is in line 12 when we pass 3 it'll be assigned to a not b , so be will remain undefined.
+
+*/
