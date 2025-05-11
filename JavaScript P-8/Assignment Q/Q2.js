c8610467863fc1d86c87528b61a9f089ad5d5b6b
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-05-11 19:12:20 +0000

    Array Methods

--- JavaScript P-8/Assignment Q/Q2.js
+++ JavaScript P-8/Assignment Q/Q2.js
@@ -1,1 +1,5 @@
 //Qs2.Create a new array using the map function whose each element is equal to the original element plus 5.
+
+let array = [2,4,6,8,10];
+let newArray = array.map(num => num +5);
+console.log(newArray);
