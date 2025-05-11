44e6ee03b61462b8ac6ea77bed998ed7c345fe0c
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-05-11 18:29:24 +0000

    Array Methods

---
+++ JavaScript P-8/Destructuring/destructuring(Object).js
@@ -0,0 +1,10 @@
+let student ={
+    name:"Shivam",
+    age: 23,
+    city :"Noida",
+    username :"Shivi j.",
+    password : 1234
+};
+
+let{ name :user , city : location} = student;
+console.log(user,location);
