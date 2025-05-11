599c7743025396cd7d58705abf4eec2538583144
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-05-11 19:23:19 +0000

    Array Methods

---
+++ JavaScript P-8/Assignment Q/Q3.js
@@ -0,0 +1,5 @@
+//Qs3.Create a new array whose elements are in uppercase of words present in the original array.
+
+let array = ["abcd","efgh"];
+let newArray = array.map(words=> words.toUpperCase());
+console.log(newArray);
