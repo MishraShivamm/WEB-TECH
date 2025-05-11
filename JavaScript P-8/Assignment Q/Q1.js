08a5e594748abc9e002b932cab4573ae6d7450df
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-05-11 18:49:46 +0000

    Array Methods

--- JavaScript P-8/Assignment Q/Q1.js
+++ JavaScript P-8/Assignment Q/Q1.js
@@ -1,5 +1,9 @@
 //Qs1.Square and sum the array elements using the arrow function and then find the average of the array.
 
 let array = [1,2,3,4];
+let newArrmul = array.map(num => num * num);
+console.log(newArrmul);
 let newArr = array.reduce((sum,el)=> sum +el);
 console.log(newArr); 
+let average = newArr/array.length;
+console.log(average); 
