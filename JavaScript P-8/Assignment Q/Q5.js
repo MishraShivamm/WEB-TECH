69c15d73f3ccb2b121c2d469d039914c4a97a30e
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-05-12 10:28:22 +0000

    Array Methods

--- JavaScript P-8/Assignment Q/Q5.js
+++ JavaScript P-8/Assignment Q/Q5.js
@@ -1,2 +1,3 @@
 // Qs5.Write a function called mergeObjects that accepts two objects and returns a new object which contains all the keys and values of the first object and second object.
 
+let mergeObjects =
