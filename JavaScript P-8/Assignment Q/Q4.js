a1676d26ddaabc6cafb20d3f3bdcd30b8f8789c3
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-05-12 10:07:47 +0000

    Array Methods

---
+++ JavaScript P-8/Assignment Q/Q4.js
@@ -0,0 +1,2 @@
+// Qs4. Write a function called doubleAndReturn Args which accepts an array and a variable number of arguments.The function should return a new array with the original array values and all of the additional arguments doubled.
+
