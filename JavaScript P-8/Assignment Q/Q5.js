e65ba52b1e4f22092cb6fef522999ce9dfa27e93
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-05-12 10:36:05 +0000

    Array Methods

--- JavaScript P-8/Assignment Q/Q5.js
+++ JavaScript P-8/Assignment Q/Q5.js
@@ -3,3 +3,9 @@
 let mergeObjects = (obj1,obj2)=>({...obj1,...obj2});
 let newobj= mergeObjects({a:1,b:2},{c:3,d:4});
 console.log(newobj);
+
+/* OR
+
+mergeObjects({a:1,b:2},{c:3,d:4});
+console.log(mergeObjects({a:1,b:2},{c:3,d:4}));
+*/
