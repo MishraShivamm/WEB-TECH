3b1d2cbfc142a495c852b6450c091ff491355ff2
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-05-14 07:16:36 +0000

    DOM

--- JavaScript P-9/intro(DOM).js
+++ JavaScript P-9/intro(DOM).js
@@ -1,4 +1,8 @@
 /* DOM (Document Object Model) :
 The DOM represents a document with a logical tree.
 It allows us to manipulate / change webpage content (HTML elements).
+
+As we link our HTML page to JS , a tree like structured will be created.
+Every page where HTML is used , document object will be generated automatically.
+If we want to access the methods and the properties of document object we use "console.dir()" instead of "console.log".
 */
