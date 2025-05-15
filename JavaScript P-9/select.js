bc9bea077bef79b12b5624def34707e6dea3e768
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-05-15 19:47:33 +0000

    DOM

--- JavaScript P-9/select.js
+++ JavaScript P-9/select.js
@@ -1,2 +1,8 @@
-//Select Element : "getElementById" => Returns the Element as an object or null (if not found).
+/* Select Element : "getElementById" => Returns the Element as an object or null (if not found).
+
+It is actually a metthod of document object.
+
+By using this method we can select Elements by their id names.
+*/
+
 
