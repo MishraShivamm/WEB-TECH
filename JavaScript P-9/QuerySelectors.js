942711c248d2baabf2a08b032e0b0a41088094da
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-05-18 19:50:15 +0000

    DOM

---
+++ JavaScript P-9/QuerySelectors.js
@@ -0,0 +1,10 @@
+/* Query Selector :
+ Allows us ti use any CSS selector.
+
+*/
+
+document.querySelector("p"); //Selects the first p element.
+document.querySelector("#myId"); //Selects the element whose id is myId.
+document.querySelector(".className"); //Selects the element whose id is myId.
+
+document.querySelectorAll('p'); // Selects all the p elements.
