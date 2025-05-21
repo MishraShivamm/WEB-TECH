d829b32092134e5c85860eecd08878a888fb5fe6
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-05-21 18:13:23 +0000

    DOM

--- JavaScript P-9/Manipulating Attributes.js
+++ JavaScript P-9/Manipulating Attributes.js
@@ -1,2 +1,3 @@
 // obj.getAttribute("atr");
 // obj.setAttribute("atr",val);
+// these are also called getters and setters : getters and setters are set of 2 functions through which we can get the value and set the value.
