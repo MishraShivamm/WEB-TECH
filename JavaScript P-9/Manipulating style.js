23b6ab05a68b8e3ed4582be9359d973a3966f190
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-05-21 18:47:03 +0000

    DOM

--- JavaScript P-9/Manipulating style.js
+++ JavaScript P-9/Manipulating style.js
@@ -2,3 +2,8 @@
 
 // We use camelcase in this for example : backgroundColor.
 
+// for example:
+let head = document.querySelector('h1');
+
+head.style.color = "blue";
+head.style.baclgroundColor = "red";
