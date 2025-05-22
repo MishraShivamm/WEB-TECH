9c9b90b68904313bc9798f27df8cdff71c18dd25
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-05-22 19:07:08 +0000

    DOM

--- JavaScript P-9/class List Property.js
+++ JavaScript P-9/class List Property.js
@@ -6,4 +6,7 @@
 *   classList.toggle() -> to toggle between add & remove
 "toggle" wale ka main kaam h add or remove ka . For example : age class name already present hai and the same name humbe toggle kiya toh first toggle will remove that perticula and then prints true or false.
 
+
+
+
 */
