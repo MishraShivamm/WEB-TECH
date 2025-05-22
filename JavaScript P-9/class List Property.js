dda8f510c5d40f288d4dc0076360d1c008583343
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-05-22 18:20:49 +0000

    DOM

---
+++ JavaScript P-9/class List Property.js
@@ -0,0 +1,9 @@
+/* obj.classList
+
+*   classList.add()  -> to add new classes
+*   classList.remove() -> to remove classes
+*   classList.contains() -> to check if class exists
+*   classList.toggle() -> to toggle between add & remove
+
+
+*/
