0c401d5396f6a92b9d97238c05c2ba880be89ee7
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-05-28 18:00:05 +0000

    DOM

--- JavaScript P-9/class List Property.js
+++ JavaScript P-9/class List Property.js
@@ -1,7 +1,10 @@
-/* obj.classList
+/* 
+   obj.classList
+
 *   classList.add()  -> to add new classes,
 *   classList.remove() -> to remove classes,
 *   classList.contains() -> to check if class exists,
 *   classList.toggle() -> to toggle between add & remove,
 "toggle" wale ka main kaam h add or remove ka . For example : age class name already present hai and the same name humbe toggle kiya toh first toggle will remove that perticula and then prints true or false.
+
 */
