9838774cf3913623b3e4cf5c049ff08b0c588483
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-06-03 19:06:20 +0000

    DOM

--- JavaScript P-9/Assignment/script.js
+++ JavaScript P-9/Assignment/script.js
@@ -1,5 +1,11 @@
-//Create a new input and button element on the page using JavaScript only. Set the text of button to “Click me”;
+// Q1. Create a new input and button element on the page using JavaScript only. Set the text of button to “Click me”;
 
 let inp = document.createElement("input");
 let btn = document.createElement("button");
 btn.innerText = "Click Me";
+
+let bdy = document.querySelector('body');
+bdy.appendChild(inp);
+bdy.appendChild(btn);
+
+//Q2. 
