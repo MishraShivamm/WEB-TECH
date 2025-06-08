7b79089759db434db3752bfd277d3343f3f29f40
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-06-08 09:07:00 +0000

    DOM

---
+++ JavaScript P-10/Random Color Generator/script.js
@@ -0,0 +1,8 @@
+let h3 = document.createElement("h3");
+h3.innerText = "Generate Color";
+h3.classList.add("head");
+let button = document.createElement('button');
+button.innerText = "Click to Generate";
+button.classList.add("button");
+let div = document.createElement('div');
+div.classList.add("div");
