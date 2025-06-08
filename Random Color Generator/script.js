c2e9eaf3b3bd7427ab84eaac0424e8fa4a366022
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-06-08 09:41:19 +0000

    DOM

--- Random Color Generator/script.js
+++ Random Color Generator/script.js
@@ -1,9 +1,9 @@
 let body = document.querySelector('body');
-let h3 = document.createElement("h3");
+let h3 = document.querySelector("h3");
 h3.innerText = "Generate Color";
 h3.classList.add("head");
 body.append(h3);
-let button = document.createElement('button');
+let button = document.querySelector('button');
 button.innerText = "Click to Generate";
 button.classList.add("button");
 body.appendChild(button);
