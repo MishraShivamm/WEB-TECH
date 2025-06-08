8edcac1e1858cf89b6c2b6defb9085d093302d2d
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-06-08 10:42:47 +0000

    DOM

--- Random Color Generator/script.js
+++ Random Color Generator/script.js
@@ -1,3 +1,21 @@
-let body = document.querySelector('body');
+let btn = document.querySelector("button")
+ btn.addEventListener("click",function (){
+    let h3 = document.querySelector("h3");
+    let randomColor = getRandomColor();
+    h3.innerText = randomColor;
+
+    let body = document.querySelector('body');
+    body.style.backgroundColor = randomColor;
+
+    console.log("Color Updated");
+ })
 
 
+ function getRandomColor (){
+    let red = Math.floor(Math.random()*255);
+    let green = Math.floor(Math.random()*255);
+    let blue= Math.floor(Math.random()*255);
+
+    let color = `rgb(${red},${green},${blue})`;
+    return color;
+ }
