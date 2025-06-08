0f6843ca3597703e66ff7510e358c8574621409e
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-06-08 09:08:45 +0000

    DOM

--- JavaScript P-10/script.js
+++ JavaScript P-10/script.js
@@ -1,33 +1,33 @@
-//DOM Events : Events are signals that something has occured.(user input/actions).
-// onclick 
-// onmouseenter
+// //DOM Events : Events are signals that something has occured.(user input/actions).
+// // onclick 
+// // onmouseenter
+
+// // let btns = document.querySelectorAll("button");
+// // for (btn of btns){
+// //     btn.onclick = function (){
+// //     console.log("Kisne Hath Lgaya");
+// //     }
+// //     btn.onmouseenter = function (){
+// //     console.log("Upr Upr se");
+// //     };
+// // }
+
+// //addEventListener :
+// //element.addEventListener(event,callback);
 
 // let btns = document.querySelectorAll("button");
 // for (btn of btns){
-//     btn.onclick = function (){
-//     console.log("Kisne Hath Lgaya");
-//     }
+//     // btn.addEventListener("dblclick",function (){
+//     // console.log("Kisne Hath Lgaya");
+//     // })
 //     btn.onmouseenter = function (){
 //     console.log("Upr Upr se");
 //     };
+//     btn.addEventListener("mouseenter",() => {
+//     btn.style.color = "purple";
+//     setTimeout(() => {
+//       btn.style.color = "";
+//     }, 900);
+//   }
+// )
 // }
-
-//addEventListener :
-//element.addEventListener(event,callback);
-
-let btns = document.querySelectorAll("button");
-for (btn of btns){
-    // btn.addEventListener("dblclick",function (){
-    // console.log("Kisne Hath Lgaya");
-    // })
-    btn.onmouseenter = function (){
-    console.log("Upr Upr se");
-    };
-    btn.addEventListener("mouseenter",() => {
-    btn.style.color = "purple";
-    setTimeout(() => {
-      btn.style.color = "";
-    }, 900);
-  }
-)
-}
