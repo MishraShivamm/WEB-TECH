7054260a1c249c2616ba4e5eca81b86c69971e30
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-06-14 18:49:51 +0000

    DOM

--- Assignment P-10/script.js
+++ Assignment P-10/script.js
@@ -1,9 +1,15 @@
 let btn = document.querySelector("button");
 let divs = document.querySelectorAll("div");
 let inp = document.querySelector("input");
+let head = document.querySelector("h2");
 
 inp.addEventListener("keypress", function (){
     console.log(inp.value ,"is pressed");
+})
+inp.addEventListener("input", function (){
+    let letter = inp.value.replace(/[^a-zA-Z]/g," ");
+    console.log(inp.value ,"is pressed");
+    head.innerText = letter;
 })
 
 window.addEventListener("scroll", function () {
