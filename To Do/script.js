f9442756106a1f9975e87d34f6e9e8bfe9bbfece
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-06-15 19:06:52 +0000

    DOM

--- To Do/script.js
+++ To Do/script.js
@@ -1,21 +1,17 @@
 // let inp = document.querySelector("input");
 // let ol = document.querySelector("ol");
 // let btn = document.querySelector("button");
-
 // btn.addEventListener("click", function(){
 //     let li = document.createElement("li");
 //     li.innerText = inp.value;
-
 //     let dltbtn = document.createElement("button");
 //     dltbtn.innerText="delete";
 //     dltbtn.classList.add("delete");
-
 //     li.appendChild(dltbtn);
 //     ol.appendChild(li);
 //     console.log("clicked");
 //     inp.value = "";
 // })
-
 // let dltbtns = document.querySelectorAll(".delete");
 // for (dltbtn of dltbtns){
 //     dltbtn.addEventListener("click", function (){
