bc251be37d353a9a8432111c8e4b465205e2deaa
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-06-15 08:36:32 +0000

    DOM

--- To Do/script.js
+++ To Do/script.js
@@ -5,6 +5,13 @@ let btn = document.querySelector("button");
 btn.addEventListener("click", function(){
     let li = document.createElement("li");
     li.innerText = inp.value;
+
+    let dltbtn = document.createElement("button");
+    dltbtn.innerText = "delete";
+    dltbtn.classList.add  = "delete";
+
+    li.appendChild(dltbtn);
+    
     ol.appendChild(li);
     console.log("clicked");
     inp.value = " ";
