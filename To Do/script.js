7c9f0424226420f49484dc5eb7965ffd5730b87f
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-06-15 08:11:03 +0000

    DOM

--- To Do/script.js
+++ To Do/script.js
@@ -2,3 +2,6 @@ let inp = document.querySelector("input");
 let ol = document.querySelector("ol");
 let btn = document.querySelector("button");
 
+btn.addEventListener("click", function(){
+    console.log("clicked");
+})
