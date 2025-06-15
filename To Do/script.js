56100d216c281fc1cd56485d34122fde1bef4eb8
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-06-15 08:56:12 +0000

    DOM

--- To Do/script.js
+++ To Do/script.js
@@ -1,26 +1,51 @@
+// let inp = document.querySelector("input");
+// let ol = document.querySelector("ol");
+// let btn = document.querySelector("button");
+
+// btn.addEventListener("click", function(){
+//     let li = document.createElement("li");
+//     li.innerText = inp.value;
+
+//     let dltbtn = document.createElement("button");
+//     dltbtn.innerText="delete";
+//     dltbtn.classList.add("delete");
+
+//     li.appendChild(dltbtn);
+//     ol.appendChild(li);
+//     console.log("clicked");
+//     inp.value = "";
+// })
+
+// let dltbtns = document.querySelectorAll(".delete");
+// for (dltbtn of dltbtns){
+//     dltbtn.addEventListener("click", function (){
+//         console.log("note deleted");
+//         let par = this.parentElement;
+//         par.remove();
+//     })
+// }
+
+
 let inp = document.querySelector("input");
 let ol = document.querySelector("ol");
 let btn = document.querySelector("button");
 
-btn.addEventListener("click", function(){
+btn.addEventListener("click", function () {
     let li = document.createElement("li");
     li.innerText = inp.value;
 
     let dltbtn = document.createElement("button");
     dltbtn.innerText = "delete";
-    dltbtn.classList.add  = "delete";
+    dltbtn.classList.add("delete");
+
+    // 🟢 Attach delete functionality immediately
+    dltbtn.addEventListener("click", function () {
+        console.log("note deleted");
+        this.parentElement.remove();
+    });
 
     li.appendChild(dltbtn);
-
     ol.appendChild(li);
     console.log("clicked");
-    inp.value = " ";
-})
-
-let dltbtns = document.querySelectorAll(".delete");
-
-for (dltbtn of dltbtns){
-    dltbtn.addEventListener("click", function (){
-        console.log("note deleted");
-    })
-}
+    inp.value = "";
+});
