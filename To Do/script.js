3870348f0375fadf8ff3b98430ab34f0114e6c10
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-06-15 19:10:38 +0000

    DOM

--- To Do/script.js
+++ To Do/script.js
@@ -23,27 +23,21 @@
 
 
 let inp = document.querySelector("input");
-
 let ol = document.querySelector("ol");
-
 let btn = document.querySelector("button");
-
 
 btn.addEventListener("click", function () {
     let li = document.createElement("li");
     li.innerText = inp.value;
 
-
     let dltbtn = document.createElement("button");
     dltbtn.innerText = "delete";
     dltbtn.classList.add("delete");
-
 
     dltbtn.addEventListener("click", function () {
         console.log("note deleted");
         this.parentElement.remove();
     });
-
 
     li.appendChild(dltbtn);
     ol.appendChild(li);
