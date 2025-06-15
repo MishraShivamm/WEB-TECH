8cd0649c02ff56780f4884b87f1d0e0319a1ec04
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-06-15 08:18:05 +0000

    DOM

--- To Do/script.js
+++ To Do/script.js
@@ -3,5 +3,9 @@ let ol = document.querySelector("ol");
 let btn = document.querySelector("button");
 
 btn.addEventListener("click", function(){
+    let li = document.createElement("li");
+    li.innerText = inp.value;
+    ol.appendChild(li);
     console.log("clicked");
+    inp.value = " ";
 })
