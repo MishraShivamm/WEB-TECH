f20e3140aaa5927dd0154b68628c970e82d1b659
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-06-28 18:48:11 +0000

    DOM

--- Simon says/script.js
+++ Simon says/script.js
@@ -3,9 +3,7 @@ let userSeq = [];
 
 let started = false;
 let level = 0;
-
 let h2 = document.querySelector("h2");
-
 document.addEventListener("keypress", function (){
     if(started == false){
         console.log("Game started");
