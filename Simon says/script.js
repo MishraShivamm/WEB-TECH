1977496a4e344b70c093fd38e65862e31e4c1cdc
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-07-03 19:26:36 +0000

    DOM

--- Simon says/script.js
+++ Simon says/script.js
@@ -1,10 +1,10 @@
 let gameSeq = [];
 let userSeq = [];
 
-
 let btns = ["yellow","red","purple","green"];
 let started = false;
 let level = 0;
+
 
 let h2 = document.querySelector("h2");
 document.addEventListener("keypress", function (){
@@ -15,7 +15,6 @@ document.addEventListener("keypress", function (){
         levelUp();
     }
 })
-
 function gameFlash(btn){
     btn.classList.add("flash");
     setTimeout(function(){
