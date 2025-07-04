c1424a53410083edd7e6aea3ced42866ef6429c8
Shivamm Mishra <91306181+MishraShivamm@users.noreply.github.com>
2025-07-04 19:08:00 +0000

    DOM

--- Simon says/script.js
+++ Simon says/script.js
@@ -14,16 +14,12 @@ document.addEventListener("keypress", function (){
     }
 })
 
-
-
 function gameFlash(btn){
     btn.classList.add("flash");
     setTimeout(function(){
         btn.classList.remove("flash");
     },250);
 }
-
-
 
 function userFlash(btn){
     btn.classList.add("userFlash");
